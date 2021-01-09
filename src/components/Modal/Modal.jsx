78653f844a6dc9fdc56ab64669bdf92cce204c69
Philip Chan <philip.chan92@icloud.com>
2021-01-09 18:03:32 +0100

    capitalize first characters of stylesheet filenames

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import whiteCross from "../../assets/white_cross.svg"
-import styles from "./modal.module.css";
-import commonStyles from "../common.module.css"
+import styles from "./Modal.module.css";
+import commonStyles from "../Common.module.css"
 import ReactModal from 'react-modal';
 
 // render reset password modal
